f7c2288303feb0bdc98cd028ea1bc61bd919beef
viktor.rozgonyi <viktor.rozgonyi@gmail.com>
2023-01-02 18:21:06 +0100

    fix: removed srcSet property

--- components/QuiltedImageList.tsx
+++ components/QuiltedImageList.tsx
@@ -6,89 +6,24 @@ import skills from '../json/skills.json';
 
 function srcset(image: string, size: number, rows = 1, cols = 1) {
   return {
-    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-    srcSet: `${image}?w=${size * cols}&h=${
-      size * rows
-    }&fit=crop&auto=format&dpr=2 2x`,
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`
   };
 }
 
 export default function QuiltedImageList() {
   return (
-    <ImageList variant="masonry" cols={8} gap={30}>
-        {skills.map((item) => (
-          <ImageListItem key={item.name}>
-            <Image
-              src={`${item.imageUrl}?w=248&fit=crop&auto=format`}
-              srcSet={`${item.imageUrl}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              alt={item.name}
-              loading="lazy"
-              width={300}
-              height={300}
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
+    <ImageList variant='masonry' cols={8} gap={30}>
+      {skills.map((item) => (
+        <ImageListItem key={item.name}>
+          <Image
+            src={`${item.imageUrl}?w=248&fit=crop&auto=format`}
+            alt={item.name}
+            loading='lazy'
+            width={300}
+            height={300}
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
   );
 }
-
-// const itemData = [
-//   {
-//     img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-//     title: 'Breakfast',
-//     rows: 2,
-//     cols: 2,
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-//     title: 'Burger',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-//     title: 'Camera',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-//     title: 'Coffee',
-//     cols: 2,
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-//     title: 'Hats',
-//     cols: 2,
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-//     title: 'Honey',
-//     author: '@arwinneil',
-//     rows: 2,
-//     cols: 2,
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-//     title: 'Basketball',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-//     title: 'Fern',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-//     title: 'Mushrooms',
-//     rows: 2,
-//     cols: 2,
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-//     title: 'Tomato basil',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-//     title: 'Sea star',
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-//     title: 'Bike',
-//     cols: 2,
-//   },
-// ];
