93d06dd6db09539026c405a56c46011f070d2d1d
viktor.rozgonyi <viktor.rozgonyi@gmail.com>
2023-01-04 00:58:43 +0100

    feat: Project cards with dynamic icons
    refs: #8

---
+++ assets/projectIcons.tsx
@@ -0,0 +1,13 @@
+export const projectIcons = {
+  'Adobe XD': '/icons/adobexd.svg',
+  CSS: '/icons/css.png',
+  Firebase: '/icons/firebase.svg',
+  HTML: '/icons/html.svg',
+  JavaScript: '/icons/javascript.svg',
+  'Material UI': '/icons/mui.svg',
+  'Next.js': '/icons/nextjs.svg',
+  'React.js': '/icons/react.svg',
+  Redux: '/icons/redux.svg',
+  Sass: '/icons/sass.svg',
+  Stripe: '/icons/stripe.svg'
+};
