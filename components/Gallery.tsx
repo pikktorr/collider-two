f15632e293d3d951c1658aee38a2b9646f298840
viktor.rozgonyi <viktor.rozgonyi@gmail.com>
2023-01-06 02:13:42 +0100

    feat: Gallery section
    - imported fslightbox.js
    - added new images and youtube videos
    
    refs: #10 #11

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,27 +1,19 @@
 import { useState } from 'react';
-import { Container, Typography } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2';
-import ProjectCard from './ProjectCard';
-import Box from '@mui/material/Box';
+import { Container, Typography, Tooltip } from '@mui/material';
 import Image from 'next/image';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import galleryList from '../json/gallery.json';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
-import ZoomInIcon from '@mui/icons-material/ZoomIn';
-import ImageDialog from './ImageDialog';
+import FsLightbox from 'fslightbox-react';
 
 const Gallery = () => {
   const [open, setOpen] = useState(false);
-  const [currentImage, setCurrentImage] = useState('');
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>();
 
-  const handleClickOpen = (imageUrl: string) => {
-    setCurrentImage(imageUrl);
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
+  const handleClickOpen = (imageUrl: number) => {
+    setOpen(!open);
+    setCurrentImageIndex(imageUrl);
   };
 
   return (
@@ -39,34 +31,40 @@ const Gallery = () => {
       </Container>
 
       <Container maxWidth='md' className='relative mt-10'>
-        <ImageList variant='masonry' cols={2} gap={12} className='gradient-shadow'>
-          {galleryList.map((item) => (
-            <ImageListItem
-              key={item.title}
-              className='hover:cursor-pointer group'
-              onClick={()=> handleClickOpen(item.url)}
-            >
-              <PlayCircleIcon className='absolute text-6xl transform -translate-x-1/2 -translate-y-1/2 rounded-full opacity-75 group-hover:opacity-100 fill-white top-1/2 left-1/2' />
-              {/* <ZoomInIcon className='absolute bottom-0 right-0 text-6xl transform -translate-x-1/2 -translate-y-1/2 rounded-full opacity-75 group-hover:opacity-100 fill-white ' /> */}
-              <Image
-                src={item.url}
-                alt={item.title}
-                width={360}
-                height={500}
-                className='border rounded-xl border-4 border-white'
-              />
-            </ImageListItem>
+        <ImageList
+          variant='masonry'
+          cols={2}
+          gap={12}
+          className='gradient-shadow'
+        >
+          {galleryList.map((item, index: number) => (
+            <Tooltip title={item.title} key={item.title} arrow>
+              <ImageListItem
+                className='hover:cursor-pointer group'
+                onClick={() => handleClickOpen(index)}
+              >
+                {item.videoUrl && (
+                  <PlayCircleIcon className='absolute text-6xl transform -translate-x-1/2 -translate-y-1/2 rounded-full shadow-md opacity-75 group-hover:opacity-100 fill-white top-1/2 left-1/2' />
+                )}
+                <Image
+                  src={item.url}
+                  alt={item.title}
+                  width={360}
+                  height={500}
+                  className='border-4 border-white rounded-xl'
+                />
+              </ImageListItem>
+            </Tooltip>
           ))}
         </ImageList>
       </Container>
-          {open && (
-            <ImageDialog
-              open={open}
-              handleClose={handleClose}
-              imageUrl={currentImage}
-              
-            />
-          )}
+
+      <FsLightbox
+        toggler={open}
+        sources={galleryList.map((item) => item.videoUrl ?? item.url)}
+        types={galleryList.map((item) => (item.videoUrl ? 'youtube' : 'image'))}
+        sourceIndex={currentImageIndex}
+      />
     </>
   );
 };
